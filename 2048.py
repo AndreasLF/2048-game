075c1f5f2619ddee5c83875b56565184ed0ced41
Andreas Fiehn <andreas@lyhnefiehn.dk>
2022-10-04 10:06:16 -0500

    Feat: Created functions to move tiles up and merge them

--- 2048.py
+++ 2048.py
@@ -44,10 +44,50 @@ class Game:
         # Update the board with a two
         self.board[random_empty_tile[0]][random_empty_tile[1]] = 2
 
+    def move_up(self):
+        # Loop through all columns on board
+        for col in range(self.board.shape[1]):
+            non_zero_rows = []   
+            # Loop through all rows and find all non zero tiles
+            for row in range(self.board.shape[0]):
+                if self.board[row][col] != 0:
+                    # make list of all non zero rows
+                    non_zero_rows.append(self.board[row][col])
+
+            # append 0s to the end of the list until len is 5
+            while len(non_zero_rows) < self.dim:
+
+                non_zero_rows.append(0)
+              
+
+            non_zero_rows = np.array(non_zero_rows)
+
+            # replace column with non_zero_rows
+            self.board[:,col] = non_zero_rows
+
+    def merge_up(self):
+        # Loop through each columns
+        for col in range(self.board.shape[1]):
+            # Loop through the rows and merge tiles
+
+            for row in range(self.board.shape[0] - 1):
+                # if the tile is not zero and the tile above is the same
+
+                if self.board[row][col] == self.board[row + 1][col]:
+                    # Multiply current tile by two (add tiles together)
+                    self.board[row][col] *= 2
+                    # Set tile below to zero
+                    self.board[row + 1][col] = 0    
+
 
 # 5x5 test board
-# board = np.array([[20,0,20,20,0],[20,20,20,0,0],[20,0,10,20,20],[20,10,0,20,0],[0,0,10,0,0]])
+board = np.array([[20,0,20,20,0],[20,20,20,0,0],[20,0,10,20,20],[20,10,0,20,0],[0,0,10,0,0]])
 
-# game = Game(5, board)
-game = Game(5)
+game = Game(5, board)
+game.print_board()
+game.move_up()
+game.print_board()
+game.merge_up()
+game.print_board()
+game.move_up()
 game.print_board()
